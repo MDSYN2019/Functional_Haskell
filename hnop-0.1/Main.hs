d5d6d310826c39ff31adcd11e0b35d837d91615d
Sang <gnatnoh123@googlemail.com>
2019-02-18 01:21:02 +0000

    First Haskell commit

---
+++ hnop-0.1/Main.hs
@@ -0,0 +1,6 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+module Main where
+
+-- | main, do nothing
+main :: IO ()
+main = return ()
---
+++ src/module1.hs
@@ -0,0 +1,9 @@
+import System.IO
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
+
+
+
